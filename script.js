179740be081731deb4d46e103f14881a07338dc8
MRROCK001 <sahil656556hay@gmail.com>
2026-05-24 01:16:04 +0530

    Add emailjs integration for contact form
    Initialize emailjs and add form submission handling.

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log("Ramesh Staffing Website Loaded");
+emailjs.init("YOUR_PUBLIC_KEY");
+
+document
+.getElementById("contact-form")
+.addEventListener("submit",function(e){
+
+e.preventDefault();
+
+emailjs.sendForm(
+"service_arc60xq",
+"YOUR_TEMPLATE_ID",
+this
+)
+
+.then(function(){
+
+alert("Message Sent Successfully");
+
+})
+
+.catch(function(error){
+
+alert("Error : "+error);
+
+});
+
+});
